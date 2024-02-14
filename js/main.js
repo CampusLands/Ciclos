1b2715cd47c7b82e752bdfcb9bc3c58118805e3c
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-02-14 07:48:19 -0500

    feat: :sparkles: Bucles
    Configuracio del while con confirm

--- js/main.js
+++ js/main.js
@@ -1,22 +1,27 @@
 
-let lista = new Array(11).fill(undefined);
-for(let i in lista){
-    console.group(`Table del ${i}`);
-    for(let x in lista){
-        console.log(`${i} X ${x} = ${Number(i)*Number(x)}`);
-    }
-    console.groupEnd("");
+
+
+
+while (confirm("Desea continuar")) {
+    console.log("Hola");
 }
 
 
-// let list = new Array(10).fill(undefined);
 
-// for (let i = 0; i < list.length; i++) {
-//     console.group(`Table del ${i}`);
-//     for (let x = 0; x < list.length; x++) {
-//         console.log(`${i} X ${x+1} = ${i*(x+1)}`);
-//     }
-//     console.groupEnd("");
-// }
+
+
+
+
+
+
+// prompt()
+// let data = prompt("Ingrese el nombre")
+// console.log(data);
+
+// alert()
+// confirm()
+// let bandera = confirm("¿ Estoy entendiendo JavaScript ?");
+// console.log(bandera);
+
 
 
